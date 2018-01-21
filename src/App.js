0692c22974b1e0760800b7fd4dd7ceaba9f6ca8f
perez6736 <perez6736@gmail.com>
2018-01-20 22:51:16 -0500

    added the render method to start doing react things

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import FriendCard from "./components/FriendCard";
 import friends from "./friends.json";
 
-const App = () => (
-  <Wrapper>
-    <Title>Friends List</Title>
-    <FriendCard
-      name={friends[0].name}
-      image={friends[0].image}
-      isClicked={friends[0].isClicked}
-    />
-    <FriendCard
-      name={friends[1].name}
-      image={friends[1].image}
-      isClicked={friends[1].isClicked}
-    />
-    <FriendCard
-      name={friends[2].name}
-      image={friends[2].image}
-      isClicked={friends[2].isClicked}
-    />
-  </Wrapper>
-);
+
+class App extends Component{
+  render(){
+    return(
+      <Wrapper>
+      <Title>Friends List</Title>
+      <FriendCard
+        name={friends[0].name}
+        image={friends[0].image}
+        isClicked={friends[0].isClicked}
+      />
+      <FriendCard
+        name={friends[1].name}
+        image={friends[1].image}
+        isClicked={friends[1].isClicked}
+      />
+      <FriendCard
+        name={friends[2].name}
+        image={friends[2].image}
+        isClicked={friends[2].isClicked}
+      />
+    </Wrapper>
+    )
+  }
+} 
 
 export default App;
